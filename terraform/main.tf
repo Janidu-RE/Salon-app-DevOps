b4c21687987bca48822686972535ab5178c7d4e2
Janidu-RE <janinduhemachandra@gmail.com>
2026-03-25 10:35:44 +0530

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -114,6 +114,8 @@ resource "aws_instance" "ubuntu_server" {
 
 }
 output "instance_public_ip" {
+  value = aws_instance.ubuntu_server.public_ip
+}
+output "instance_Elastic_ip" {
   value = aws_eip.lb.public_ip
 }
-
